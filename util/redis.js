01c32cf05c2ab9c29b7a7ebb41c0bd3dbe8bb4de
Udit Singh <43987867+udit1707@users.noreply.github.com>
2021-06-17 00:04:47 +0530

    Delete util directory

--- util/redis.js
+++
@@ -1,6 +0,0 @@
-const redis = require("redis");
-
-const redisClient=redis.createClient(6380, process.env.REDISCACHEHOSTNAME,
-    {auth_pass: process.env.REDISCACHEKEY, tls: {servername: process.env.REDISCACHEHOSTNAME}});
-
-module.exports=redisClient;
